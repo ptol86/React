2a384a50e4d0849e0d86afeef5cceeb149853f01
ptol86 <ah021714@gmail.com>
2020-07-02 13:55:08 +0300

    test

---
+++ lesson1/task5/review_build/precache-manifest.773ec320a3b72937d7ed6a85b5b2bc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63de44f943734055f57d3a860a40137a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "261f94596a14ac7bff1b",
+    "url": "/static/css/main.eb44c19c.chunk.css"
+  },
+  {
+    "revision": "f4cd8603cbc98fa8da98",
+    "url": "/static/js/2.730fe4ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.730fe4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "261f94596a14ac7bff1b",
+    "url": "/static/js/main.94ed2f34.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  }
+]);
