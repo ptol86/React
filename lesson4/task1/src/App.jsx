719412abd27e094cf3cbacdef3c87eb9bd085c51
ptol86 <ah021714@gmail.com>
2020-07-05 21:11:33 +0300

    test

---
+++ lesson4/task1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Counter from './Counter'
+
+const App = () => {
+    return (
+        <>
+        <Counter start={17} interval={100}/>
+        <Counter start={-50} interval={1000}/>
+        <Counter start={5} interval={3000}/>
+        </>
+    )
+}
+
+export default App;
