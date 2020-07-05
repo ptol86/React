97bd22ee563216464e3496b205d4f8dcefafbd49
ptol86 <ah021714@gmail.com>
2020-07-05 19:19:15 +0300

    test

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import moment from "moment";
-import {Component} from "react";
 
-class Profile extends Component { 
-    
-    render() {
-        const age = moment(this.props.birthDate).format("d MMM YY");
+
+const Profile = (props) => { 
         
-        return (
-            <div className="profile">
-                <div className="profile__name">
-                    {`${this.props.firstName} ${this.props.lastName}`}
-                </div>
-                <div className="profile__birth">
-                    {`Was born ${age} in ${this.props.birthPlace}`}
-                </div>
+    const age = moment(props.birthDate).format("d MMM YY");
+    
+    return (
+        <div className="profile">
+            <div className="profile__name">
+                {`${props.firstName} ${props.lastName}`}
             </div>
-        );
-    };
+            <div className="profile__birth">
+                {`Was born ${age} in ${props.birthPlace}`}
+            </div>
+        </div>
+    );
+
 }
 export default Profile;
