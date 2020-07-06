931930d46fbbc34fe0cf508f534290926593dcbb
ptol86 <ah021714@gmail.com>
2020-07-06 17:19:14 +0300

    test

---
+++ lesson5/task1/src/GoodButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const GoodButton = () => {
+   return <>
+    <button 
+        className="fancy-button"
+        onClick={() => alert('Good job!')}
+    >
+        Click me!
+    </button>
+    </>
+}
+
+export default GoodButton;
