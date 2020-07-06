afb750635120fa341e611f95ec73b9f1c22c54d3
ptol86 <ah021714@gmail.com>
2020-07-06 19:34:06 +0300

    test

--- lesson5/task3/src/Counter.jsx
+++ lesson5/task3/src/Counter.jsx
@@ -9,13 +9,25 @@ class Counter extends React.Component {
 
         }
 
-        this.decrement =
+        this.decrement = this.decrement.bind(this);
     }
 
     decrement() {
         this.setState({
             counter: this.state.counter -1,
-        })
+        });
+    }
+
+    increment = () => {
+        this.setState({
+            counter: this.state.counter +1,
+        });
+    }
+
+    reset() {
+        this.setState({
+            counter: 0,
+        });
     }
 
     render() {
@@ -23,13 +35,18 @@ class Counter extends React.Component {
             <div className="counter">
                 <button data-action="decrease"
                  className="counter__button"
-                 onClick={this.decrement()}>
+                 onClick={this.decrement}>
                     -
                 </button>
-                <span className="counter__value">
+                <span className="counter__value"
+                onClick={() => this.reset()}
+                >
                     {this.state.counter}
                 </span>
-                <button data-action="increase" className="counter__button">
+                <button data-action="increase" 
+                className="counter__button"
+                onClick={this.increment}
+                >
                     +
                 </button>
             </div>
