9f7884b2814de6ebf2a6d41b7f7b79562c43acca
ptol86 <ah021714@gmail.com>
2020-07-07 18:42:28 +0300

    test

---
+++ lesson6/task1/src/Greeting.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import UserGreeting from './UserGreeting';
+import GuestGreeting from './GuestGreeting';
+
+const Greeting = (props) => {
+    if (props.isLoggedIn) {
+        return <UserGreeting/>;
+    }
+    return <GuestGreeting/>;
+}
+export default Greeting;
