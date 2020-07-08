9f3e204a4c278d9a742f219a6fa21a34bce7f570
ptol86 <ah021714@gmail.com>
2020-07-08 18:27:49 +0300

    test

--- lesson7/task3/src/Transaction.jsx
+++ lesson7/task3/src/Transaction.jsx
@@ -8,8 +8,8 @@ const Transaction = ({from, to, amount, rate, time}) => {
             <span className="transaction__date">{moment(time).format("DD MMM")}</span>
             <span className="transaction__time">{moment(time).format("hh:mm")}</span>
             <span className="transaction__assets">{`${from} → ${to}`}</span>
-            <span className="transaction__rate">{rate}</span>
-            <span className="transaction__amount">{amount}</span>
+            <span className="transaction__rate">{new Intl.NumberFormat('en-GB').format(rate)}</span>
+            <span className="transaction__amount">{new Intl.NumberFormat('en-GB').format(amount)}</span>
         </li>
     );
         
