d9ae28249923969905c2529be8984a35a89f2ce4
ptol86 <ah021714@gmail.com>
2020-07-09 22:04:32 +0300

    test

--- lesson8/task5/src/ConnectionStatus.jsx
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -4,38 +4,35 @@ class ConnectionStatus extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            status: "online",
+          status: 'online',
         }
-    }
-    componentDidMount() {
-        window.addEventListener("online", this.handleStatusOnline);
-        window.addEventListener("offline", this.handleStatusOffline);
-    }
-    componentWillUnmount() {
-        window.removeEventListener("online", this.handleStatusOnline);
-        window.removeEventListener("offline", this.handleStatusOffline);
-    }
-    handleStatusOnline = () => {
-        this.setState ({
-            status: "online"
+      }
+      componentDidMount(){
+        window.addEventListener('online', this.handleStatusOnline);
+        window.addEventListener('offline', this.handleStatusOffline);
+      }
+      componentWillUnmount(){
+        window.removeEventListener('online', this.handleStatusOnline);
+        window.removeEventListener('offline', this.handleStatusOffline);
+      }
+      handleStatusOnline = () => {
+        this.setState({
+          status: 'online',
+        })
+      }
+      handleStatusOffline = () => {
+        this.setState({
+          status: 'offline',
         });
+      };
+    render () {
+      const { status } = this.state;
+      return (
+        <div className={`status ${status === "offline" ? "status_offline" : ''}`}
+        >
+           {status}
+        </div>
+      );
     }
-    handleStatusOffline = () => {
-        this.setState ({
-            status: "offline"
-        });
-    }
-
-
-    render() {
-        const {status} = this.state;
-
-        return (
-            <div className={`status ${status === "offline"?"status_offline":""}`}>
-                {status}
-            </div>
-        )
-    }
-};
-
-export default ConnectionStatus;
+  }
+  export default ConnectionStatus;
