16c6acc18fa04a9a24c117fc8169abdae8195604
ptol86 <ah021714@gmail.com>
2020-07-13 17:34:51 +0300

    test

--- lesson9/task5/src/index.jsx
+++ lesson9/task5/src/index.jsx
@@ -44,8 +44,7 @@ function Square(props) {
       );
     }
   }
-  
-  class Game extends React.Component {
+export default class Game extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
