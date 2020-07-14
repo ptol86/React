09454e307fcf89e84444cf449cea1763d9687279
ptol86 <ah021714@gmail.com>
2020-07-14 16:09:01 +0300

    test

--- lesson10/task2/src/App.jsx
+++ lesson10/task2/src/App.jsx
@@ -24,7 +24,7 @@ class Page extends React.Component {
         const { userData } = this.state;
         return (
             <div className="page">
-                <h1 className="title">{`Hello, ${userData.firstName}`}</h1>
+                <h1 className="title">{`Hello, ${userData.firstName} ${userData.lastName}`}</h1>
                 <main className="content">
                     <ShoppingCart userName={this.state.userData.firstName}/>
                     <Profile userData={userData} handleChange={this.handleChange}/>
