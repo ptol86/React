88247ca562cca36946d84de38e28ae385bc85c7c
ptol86 <ah021714@gmail.com>
2020-07-14 13:36:54 +0300

    test

---
+++ lesson10/task1/src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import UserProfile from './UserProfile'
+import UserMenu from './UserMenu'
+
+
+class Page extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userData: null
+        }
+    }
+
+    componentDidMount() {
+        this.fetchUserData(this.props.userId);
+    }
+
+    fetchUserData = userId => {
+        const userUrl = `https://api.github.com/users/${userId}`
+        fetch(userUrl)
+        .then(response => response.json())
+        .then(userData => this.setState({
+            userData,
+        }))
+    }
+    render() {
+        return (
+        <div className="page">
+            <header className="header">
+                <UserMenu userData = {this.state.userData}/>
+            </header>
+                <UserProfile userData = {this.state.userData}/>
+        </div>
+        )
+    }
+}
+    
+export default Page; 
