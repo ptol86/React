87c6ce18616d9e9e47f73411afebe134ea6b09a3
ptol86 <ah021714@gmail.com>
2020-07-16 14:39:41 +0300

    test

---
+++ lesson11/task1/src/Numbers.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Numbers extends React.PureComponent {
+   
+    render() {
+        return (
+            <div className="number">
+                <span 
+                className="number__title">
+                {this.props.title}
+                </span>
+                <span 
+                className="number__value">
+                {this.props.number}
+                </span>
+            </div>
+        )
+    }
+}
+Numbers.propTypes = {
+    title: PropTypes.string,
+    number: PropTypes.number.isRequired,
+}
+Numbers.defaultProps = {
+    title: "My title",
+}
+
+export default Numbers;
