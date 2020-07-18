f87b316642abab55ec135f4ba4f343d9df9a41d7
ptol86 <ah021714@gmail.com>
2020-07-18 13:23:28 +0300

    test

--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 
-const Expand = ({ children, title, onOpen, onClose, isVisible }) => {
-    if (!isVisible) return (
-        <div className="expand border">
-            <div className="expand__header">
-            {title}
-            <button className="expand__toggle-btn" onClick={onOpen}>
-                <i className="fa fa-chevron-down"></i>
-            </button>
-            </div>
-            
-        </div>
-    );
+class Expand extends React.Component {
+    state = { 
+        isVisible: false, 
+    }
 
-    return (
-        <div className="expand border">
-            <div className="expand__header">
-            {title}
-            <button className="expand__toggle-btn" onClick={onClose}>
-                <i className="fa fa-chevron-up"></i>
-            </button>
+    toggleButton = () => {
+        this.setState({
+            isVisible: !this.state.isVisible,
+        })
+    }
+    
+    render() {
+        const {title, children} = this.props;
+        const {isVisible} = this.state;
+        return (
+            <div className="expand border">
+                <div className="expand__header">
+                {title}
+                <button className="expand__toggle-btn" onClick={this.toggleButton}>
+                    {isVisible && <i className="fa fa-chevron-up"></i> || <i className="fa fa-chevron-down"></i>}
+                </button>
+                </div>
+                {isVisible && <div className="expand__content">
+                    {children}
+                </div>}
             </div>
-            <div className="expand__content">
-                {children}
-            </div>
-        </div>
-    )
+        )
+    }
 
 }
 
