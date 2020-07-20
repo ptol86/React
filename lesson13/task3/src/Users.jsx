5969f51156af9fbfa1934028395d6883d79d4a1b
ptol86 <ah021714@gmail.com>
2020-07-20 11:33:06 +0300

    test

---
+++ lesson13/task3/src/Users.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import User from "./User";
+import { Link, Route, Switch } from "react-router-dom";
+
+const Users = ({match}) => {
+    return (
+      <div className="page__content">
+          <h1>Products</h1>
+          <ul className="navigation">
+              <li className="navigation__item">
+                <Link to={`${match.url}/github`}>Github</Link>
+              </li>
+              <li className="navigation__item">
+                <Link to={`${match.url}/facebook`}>Facebook</Link>
+              </li>
+          </ul>
+          <Switch>
+            <Route exact path={match.url} >
+              <span>Select a user please</span>
+            </Route>
+            <Route path={`${match.url}/:userId`}>
+              <User />
+            </Route>
+          </Switch>
+      </div>
+    );
+  
+}
+
+export default Users; 
