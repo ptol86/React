2a5e66f1c11c0e3db46d63fe3ed9772f7e9c8649
ptol86 <ah021714@gmail.com>
2020-07-22 18:41:18 +0300

    test

---
+++ lesson14/task6/src/Clock.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import moment from 'moment';
+import { useState, useEffect } from 'react';
+
+const getTimeWithOffset = offset => {
+    const currentTime = new Date();
+    const utcOffset = currentTime.getTimezoneOffset() / 60;
+    return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+}
+
+const Clock = ({offset, location}) => {
+    
+    const [time, setTime] = useState(moment(getTimeWithOffset(offset)).format("h:mm:ss A"))
+    
+    useEffect(() => {
+        
+        setInterval(() => {
+            setTime(
+                moment(getTimeWithOffset(offset)).format("h:mm:ss A")
+            )
+        }, 1000);
+
+    }, [])
+    
+    
+    return (
+    <div className="clock">
+        <div className="clock__location">
+            {location }
+        </div>
+        <div className="clock__time">
+            {time}
+        </div>
+    </div>
+    );
+    
+};
+
+export default Clock;
+
